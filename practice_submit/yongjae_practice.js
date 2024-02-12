e689ad5eb68e1aa9dd59fb42c315c28490de5a8b
siriusbug <108792039+siriusbug@users.noreply.github.com>
2024-02-12 11:51:06 +0900

    Update yongjae_practice.js

--- practice_submit/yongjae_practice.js
+++ practice_submit/yongjae_practice.js
@@ -96,16 +96,123 @@
 // prompt() is a method of the Global window object found in browsers. It's not part of the JavaScript language. If you don't run your code in a browser or an environment that supports the Browser Object Model, window won't be there and if window isn't there, neither will prompt() be. Run your code in a browser.
 // prompt 안되네..
 
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().trim();
-var n = Number(input); 
-if(input%3 == 0) {
-    console.log('짝');
-}
-else {
-    console.log(i);
-}
+// var fs = require('fs');
+// var input = fs.readFileSync('/dev/stdin').toString().trim();
+// var n = Number(input); 
+// if(input%3 == 0) {
+//     console.log('짝');
+// }
+// else {
+//     console.log(i);
+// }
 
 // 15
+// 스키장 가는 기차 안에서..
+// npm init, npm install prompt-sync, require("prompt-sync"); 로 입력 문제 해결
+// const prompt = require("prompt-sync")();
+// const input = prompt();
+// console.log('안녕하세요. 저는 '+input+'입니다.');
 
-제가 내일까지.. 늦더라도 다음 회의 전까지 다 해서 올리겠습니다.. 망할 입력 아오 그냥
+// 16
+// const prompt = require("prompt-sync")();
+// var input = prompt();
+// var ans = Array.from(input).reverse();
+// var real_ans = '';
+// ans.forEach(letter => {
+//     real_ans += letter;
+// });
+// console.log(real_ans);
+
+// 17
+// const prompt = require("prompt-sync")();
+// const input = prompt();
+// if(input >= 150) {
+//     console.log('YES');
+// }
+// else {
+//     console.log('NO');
+// }
+
+// 18
+// const prompt = require("prompt-sync")();
+// const input = prompt();
+// const num = Array.from(input);
+// let arr = [];
+// var tmp = 0;
+// num.forEach(function(item){
+//     if('0'<=item && item<='9') {
+//         tmp*=10;
+//         tmp+=Number(item);
+//     }
+//     else {
+//         arr.push(tmp);
+//         tmp = 0;
+//     }
+// })
+// arr.push(tmp);
+// const sum = arr.reduce(function(preValue, curValue){
+//     return preValue + curValue;
+// })
+// console.log(sum/arr.length);
+
+// 19
+// const prompt = require("prompt-sync")();
+// const input = prompt();
+// const num = Array.from(input);
+// let arr = [];
+// var tmp = 0;
+// num.forEach(function(item){
+//     if('0'<=item && item<='9') {
+//         tmp*=10;
+//         tmp+=Number(item);
+//     }
+//     else {
+//         arr.push(tmp);
+//         tmp = 0;
+//     }
+// })
+// arr.push(tmp);
+
+// console.log(arr[0] ** arr[1]);
+
+// 20
+// const prompt = require("prompt-sync")();
+// const input = prompt();
+// const num = Array.from(input);
+// let arr = [];
+// var tmp = 0;
+// num.forEach(function(item){
+//     if('0'<=item && item<='9') {
+//         tmp*=10;
+//         tmp+=Number(item);
+//     }
+//     else {
+//         arr.push(tmp);
+//         tmp = 0;
+//     }
+// })
+// arr.push(tmp);
+
+// console.log(arr[0]/arr[1], arr[0]%arr[1]);
+
+// 21
+// 모르겠다.. 느낌은 1 2 3 5 맞고 4 틀린거 같지만, 이럴거면 틀린걸 고르라고 하지 왜.. 12가 틀릴지도
+
+// 22
+// 2
+
+// 23
+// O
+// console.log(10/3); 뭐야 아니네.. 근본없다
+
+// 24
+// const prompt = require("prompt-sync")();
+// const input = prompt().toUpperCase();
+// console.log(input);
+
+// 25
+// const prompt = require("prompt-sync")();
+// const input = Number(prompt());
+// console.log(input*input*3.14);
+
+
